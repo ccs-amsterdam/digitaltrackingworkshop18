3a045148f14b441159cdb1c3c0d686dd294d776d
Kasper Welbers <kasperwelbers@gmail.com>
2018-08-01 09:24:40 +0200

    made html file self contained (including css and embedded images)

--- build.r
+++ build.r
@@ -1,12 +1,22 @@
 source('build/lib.r')
 
-build_html(title = 'Digital Tracking Workshop 2018',
+css = create_css(top_button_col='#0089d1',
+                 top_button_border_col='#0089d1', 
+                 intro_col='#FFFFFF', 
+                 prepare_col = '#FFFFFF',
+                 intro_bg_col='#34495e',
+                 prepare_bg_col='#34495e',
+                 body_col='#34495e',
+                 a_col='#0089d1')
+
+build_html(css = css,
+           title = 'Digital Tracking Workshop 2018',
            author = 'Wouter van Atteveldt, Kasper Welbers and Magdalena Wojcieszak',
            date = 'October 27-28th, 2018',
            city = 'Amsterdam',
            country = 'NL',
            byline = 'Invited participants only',
-           logo = 'images/ccs_amsterdam.png',
+           logo = embed_image('images/ccs_amsterdam.png'),
            image = 'http://ccs-amsterdam.org/wp-content/uploads/2018/03/cropped-Europe-network-1024x400.jpg',
            url = 'http://ccs.amsterdam',
            intro = readtxt('build/intro.txt'),
@@ -19,12 +29,5 @@ build_html(title = 'Digital Tracking Workshop 2018',
            github = 'ccs-amsterdam/digitaltrackingworkshop18')
 
 
-build_css(top_button_col='#0089d1',
-          top_button_border_col='#0089d1', 
-          intro_col='#FFFFFF', 
-          prepare_col = '#FFFFFF',
-          intro_bg_col='#34495e',
-          prepare_bg_col='#34495e',
-          body_col='#34495e',
-          a_col='#0089d1')
+
   
--- build/lib.r
+++ build/lib.r
@@ -7,7 +7,9 @@ sink_file <- function(x, filename='index.html') {
 
 readtxt <- function(fn) readChar(fn, file.info(fn)$size)
 
-build_html <- function(title, author, date, city, country, byline, logo, image, url, intro, prepare, participants_lead, location, google_maps, sponsors, footer, github) {
+embed_image <- function(fname) paste0('data:image/png;base64,', base64enc::base64encode('images/ccs_amsterdam.png'))
+
+build_html <- function(css, title, author, date, city, country, byline, logo, image, url, intro, prepare, participants_lead, location, google_maps, sponsors, footer, github) {
   a = as.list(environment())
   
   html = readtxt('build/index_template.html')
@@ -18,12 +20,13 @@ build_html <- function(title, author, date, city, country, byline, logo, image, 
 }
 
 
-build_css <- function(top_button_col='#27ae60', top_button_border_col='#27ae60', intro_col='#183244', intro_bg_col='#ABBECD', body_col='#34495e', a_col='#2980b9', prepare_col=intro_col, prepare_bg_col=intro_bg_col) {
+create_css <- function(top_button_col='#27ae60', top_button_border_col='#27ae60', intro_col='#183244', intro_bg_col='#ABBECD', body_col='#34495e', a_col='#2980b9', prepare_col=intro_col, prepare_bg_col=intro_bg_col) {
   a = as.list(environment())
   
   css = readtxt('build/css_template.css')
   for (i in seq_along(a)) {
     css = gsub(paste0('{', toupper(names(a)[i]), '}'), a[[i]], css, fixed = T)
   }
-  sink_file(css, 'style/main.css')
+  #sink_file(css, 'style/main.css')
+  css
 }
